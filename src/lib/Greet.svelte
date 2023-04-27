ce0e73d93d892421415aeb5e2f158e35248769ad
HEIGE-PCloud <heige.pcloud@outlook.com>
2023-04-27 13:29:51 +0100

    feat: implement a simple detection for git installation

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri"
 
-  let name = "";
-  let greetMsg = ""
-
-  async function greet(){
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+  let gitInstallationMessage = "Loading...";
+  async function getGitInstallationInfo() {
+    if (await invoke("is_git_installed")) {
+      gitInstallationMessage = "git is installed!"
+    } else {
+      gitInstallationMessage = "git is not installed!"
+    }
   }
+  getGitInstallationInfo()
 </script>
 
 <div>
-  <div class="row">
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button on:click={greet}>
-      Greet
-    </button>
-  </div>
-  <p>{greetMsg}</p>
+  <p>{gitInstallationMessage}</p>
 </div>
